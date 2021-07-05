e806f159515cbb3ada44ffff91f0a18b097a4d39
LCLP <lukas@lclpnet.work>
2021-07-05 18:41:26 +0200

    shorthand xml

--- src/renderer/components/pages/Home.tsx
+++ src/renderer/components/pages/Home.tsx
@@ -13,8 +13,8 @@ class Home extends Component {
                         <div className="card-body p-0">
                             <div id="featuredSlider" className="carousel slide" data-bs-ride="carousel">
                                 <div className="carousel-indicators">
-                                    <button type="button" data-bs-target="#featuredSlider" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
-                                    <button type="button" data-bs-target="#featuredSlider" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                                    <button type="button" data-bs-target="#featuredSlider" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1" />
+                                    <button type="button" data-bs-target="#featuredSlider" data-bs-slide-to="1" aria-label="Slide 2" />
                                 </div>
                                 <div className="carousel-inner">
                                     <div className="carousel-item active">
@@ -34,11 +34,11 @@ class Home extends Component {
                                     </div>
                                 </div>
                                 <button className="carousel-control-prev" type="button" data-bs-target="#featuredSlider" data-bs-slide="prev">
-                                    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                                    <span className="carousel-control-prev-icon" aria-hidden="true" />
                                     <span className="visually-hidden">Previous</span>
                                 </button>
                                 <button className="carousel-control-next" type="button" data-bs-target="#featuredSlider" data-bs-slide="next">
-                                    <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                                    <span className="carousel-control-next-icon" aria-hidden="true" />
                                     <span className="visually-hidden">Next</span>
                                 </button>
                             </div>
