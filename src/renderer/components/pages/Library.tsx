c6bdd60a9ac5b5f164841ca4a15e6d7dca949039
LCLP <lukas@lclpnet.work>
2021-07-23 19:27:10 +0200

    library store icon

--- src/renderer/components/pages/Library.tsx
+++ src/renderer/components/pages/Library.tsx
@@ -3,8 +3,9 @@ import { Switch, Route, NavLink, Redirect, RouteComponentProps } from 'react-rou
 import Apps from './library/Apps';
 import Search from './library/Search';
 
-import '../../style/pages/library.css';
+import '../../style/pages/library.scss';
 import AppStorePage from './library/store/AppStorePage';
+import { getBackendHost } from '../../../common/settings';
 
 class Library extends Component {
     render() {
@@ -12,15 +13,16 @@ class Library extends Component {
             <>
                 <div id="libraryContainer">
                     <div id="librarySidebar" className="separator-border-dark-right">
-                        <div className="py-2">
+                        <div className="sidebar-padding">
                             <SideTab route="/library/apps" icon="apps" title="Apps" />
                             <SideTab route="/library/search" icon="search" title="Search" />
                         </div>
                         <Route exact path="/library/store/app/:app" render={props => (
-                            <div className="py-2 separator-border-dark-top">
+                            <div className="py-1 separator-border-dark-top">
                                 {
                                     React.createElement(SideAppTab, {
                                         link: `/library/store/app/${props.match.params.app}`,
+                                        title: 'Store',
                                         ...props
                                     } as SideAppTabProps)
                                 }
@@ -65,7 +67,8 @@ interface MatchParams {
 }
 
 interface SideAppTabProps extends RouteComponentProps<MatchParams> {
-    link: string
+    link: string,
+    title?: string,
 }
 
 class SideAppTab extends Component<SideAppTabProps> {
@@ -73,8 +76,10 @@ class SideAppTab extends Component<SideAppTabProps> {
         return (
             <NavLink to={this.props.link} className="navigation-link-color-dimmed no-underline" activeClassName="active">
                 <div className="tab-btn py-1">
-                    <span className="material-icons tab-icon">shopping_cart</span>
-                    <div className="tab-desc">{this.props.match.params.app}</div>
+                    <img src={`${getBackendHost()}/api/lclplauncher/apps/assets/app-icon/${this.props.match.params.app}`} className={this.props.title ? 'tab-img-sm' : 'tab-img'} alt="App icon" />
+                    {
+                        this.props.title ? (<div className="tab-desc-less">{this.props.title}</div>) : undefined
+                    }
                 </div>
             </NavLink>
         );
